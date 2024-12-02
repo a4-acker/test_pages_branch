8cec534ffd34ec456d7be1feca6145aab908228f
a4-acker <acker_you@a4tech.com.tw>
2024-12-02 10:44:50 +0800

    version

---
+++ test_pages_branch/src/scripts/exportEnv.tsx
@@ -0,0 +1,6 @@
+import { version, date, basePath } from '../config'; // 引入 config.ts 中的變數
+
+
+console.log(`VERSION=${version}`);
+console.log(`DATE=${date}`);
+console.log(`BASE_PATH=${basePath}`);
